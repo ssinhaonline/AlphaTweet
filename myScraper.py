f5a2627f4df0b785e48900f8e0e1d903ceab920e
Souradeep Sinha <ssinhaonline@gmail.com>
2015-11-15 17:11:17 -0500

    Added myScraper.py - scrapes user and dumps to <username>.csv. Next step spawn threads for followers list and call the same function.

---
+++ myScraper.py
@@ -0,0 +1,60 @@
+"""
+This module uses BeautifulSoup 4 and Requests package to scrape Twitter for data, and can be used to bypass the REST API request ceiling set by Twitter
+
+@author ssinhaonline <homepage = "www.datanoobjournals.com/ssinhaonline" email = "ssinhaonline@gmail.com">
+@date Nov 6, 2015
+"""
+
+import re
+from bs4 import BeautifulSoup
+import webbrowser
+import pdb
+
+def getTweets(usrNm):
+    """Collects the tweets of passed @username and returns a list of tweets"""
+    usrUrl = 'https://twitter.com/' + usrNm.lower()
+    src = sourceExtractor(usrUrl)
+    soup = BeautifulSoup(src)
+    tweetStream = soup.find('ol', {'id': 'stream-items-id'}).find_all('li', {'data-item-type': 'tweet'})
+    pdb.set_trace()
+    tweetdivs = []
+    for tweetTree in tweetStream:
+        tweetdivs.append(tweetTree.find('div', {'class': 'tweet'}))
+    tweetlist = []
+    data = open(usrNm + '.csv', 'w')
+
+    for item in tweetdivs:
+        try:
+            content = item.find('div', {'class': 'content'}).find('p', {'class': 'tweet-text', 'lang': 'en'}).text.replace('\n', ' ').replace('"', '')
+            timestamp = item.find('a', {'class': 'tweet-timestamp'})['title']
+            try:
+                data.write(timestamp + '|' + content + '\n')
+            except:
+                pass
+        except:
+            pass
+    data.close()
+    print 'File wriiten: ' + data.name
+    #return tweets
+
+def sourceExtractor(url):
+    from selenium import webdriver
+    from time import sleep
+    driver = webdriver.Firefox()
+    driver.get(url)
+    src1 = driver.page_source
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+    sleep(2)
+    src2 = driver.page_source
+    while(len(src1)<len(src2)):
+        src1 = src2
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        sleep(2)
+        src2 = driver.page_source
+    driver.close()
+    return src2
+
+usrNm = raw_input('Provide your username: @')
+followers = []
+usrTweets = getTweets(usrNm)
+
