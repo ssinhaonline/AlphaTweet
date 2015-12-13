9fd07d837c6c952ddec08a39c62e022f29eeaf42
Souradeep Sinha <ssinhaonline@gmail.com>
2015-12-12 19:08:30 -0500

    Mostly done. Classification task complete. Still need to figure out error calculation.

--- myDriver.py
+++ myDriver.py
@@ -3,6 +3,22 @@ import csv
 import datetime as dt
 import os
 import pdb
+from nltk.corpus import stopwords
+
+cachedStopwords = stopwords.words('english')
+
+def wordify(tokenized_tweet, tweet_list):
+    tweet_words = []
+    rt_dict = {}
+    for tweet in tweet_list:
+        tweet_words.append(tweet[1])
+    for tweetword in tokenized_tweet:
+        word_count = 0
+        for retweet in tweet_words:
+            if tweetword in retweet:
+                word_count += 1
+        rt_dict[tweetword] = word_count
+    return rt_dict
 
 def read_and_parse(filename):
     import csv
@@ -15,53 +31,134 @@ def read_and_parse(filename):
         for row in lines:
             newlines.append(row)
     except:
-        #print "In " + csvfile.name
-        #print row
-        #raise
         pass
     return [csvfile.name[2:-4], newlines]
-        
-infofile = glob('*.info')
-with open(infofile[0], 'r') as u:
-    userinfo = u.readline().strip()
-print userinfo
-os.chdir('./' + userinfo)
-userfile = glob('u_*.csv')
-userdata = read_and_parse(userfile[0])
-#print userdata
 
-followerfiles = glob('f_*.csv')
-followerdata_temp = []
-follower_tweet_map = {}
-for filename in followerfiles:
-    followerdata_temp.append(read_and_parse(filename))
-for follower in followerdata_temp:
-    follower_tweet_map[follower[0]] = follower[1]
-count = 0
-for follower in follower_tweet_map:
-    tweet_list = follower_tweet_map[follower]
-    count +=1
-    #print follower
-    #print follower_tweet_map
+def divide_dataset(tweet_list):
     train_tweet_list = []
     test_tweet_list = []
+    
+    divpoint = len(tweet_list)/4
     for tweet in tweet_list:
-        tmstmp = tweet[0]
-        twttxt = tweet[1]
-        dt_tmstmp = dt.datetime.strptime(tmstmp, '%I:%M %p - %d %b %Y')
-        if ((dt.datetime.now() - dt_tmstmp).total_seconds()/86400) <= 28.0:
-            if ((dt.datetime.now() - dt_tmstmp).total_seconds()/86400) <= 7.0:
-                test_tweet_list.append(twttxt)
-            else:
-                train_tweet_list.append(twttxt)
+        train_tweet_list = tweet_list[divpoint:]
+        test_tweet_list = tweet_list[:divpoint]
+    for i in range(len(train_tweet_list)):
+        train_tweet_list[i][1] = train_tweet_list[i][1].lower().split()
+    for i in range(len(test_tweet_list)):
+        test_tweet_list[i][1] = test_tweet_list[i][1].lower().split()
+
+    return [train_tweet_list, test_tweet_list]
+
+def get_followers_data():
+    followerfiles = glob('f_*.csv')
+    followerdata_temp = []
+    follower_tweet_map = {}
+    for filename in followerfiles:
+        followerdata_temp.append(read_and_parse(filename))
+    for follower in followerdata_temp:
+        if len(follower[1]) > 0:
+            follower_tweet_map[follower[0]] = follower[1]
+    count = 0
+    going4del = []
+    for follower in follower_tweet_map:
+        tweet_list = follower_tweet_map[follower]
+        count +=1
+        train_tweet_list, test_tweet_list = divide_dataset(tweet_list)
+        if not (len(train_tweet_list) == 0 or len(test_tweet_list) == 0):
+            follower_tweet_map[follower] = [train_tweet_list, test_tweet_list]
         else:
-            continue
-    follower_tweet_map[follower] = [train_tweet_list, test_tweet_list]
+            going4del.append(follower)
+    for follower in going4del:
+        deletefol = follower_tweet_map.pop(follower)
+    
+    return follower_tweet_map
 
-for follower in follower_tweet_map:
-    print follower
-    print 'Train set = ' + str(len(follower_tweet_map[follower][0]))
-    print 'Test set = ' + str(len(follower_tweet_map[follower][1]))
+def get_all_data(username):
+    infofile = glob(username + '.info')
+    with open(infofile[0], 'r') as u:
+        userinfo = u.readline().strip()
+    os.chdir('./' + userinfo)
+    userfile = glob('u_*.csv')
+    userwholedata = read_and_parse(userfile[0])
+    user_ent = {userwholedata[0] : divide_dataset(userwholedata[1])}
+    followers_ent = get_followers_data()
+    return [user_ent, followers_ent]
 
-[os.remove(f) for f in glob('*.jpg')]
-os.chdir('../')
+def classify(user, input_tweet, user_tweets, follower_tweets):
+    input_tweet = [word for word in input_tweet if word not in cachedStopwords]
+    follower_wordified = []
+    for follower in follower_tweets:
+        follower_wordified.append(wordify(input_tweet, follower_tweets[follower][0]))
+    wordifies = {}
+    for key in follower_wordified[0]:
+        wordifies[key] = 0
+    for row in follower_wordified:
+        for key in wordifies:
+            wordifies[key] += row[key]
+    for follower in follower_wordified:
+        for key in wordifies:
+            if wordifies[key] != 0:
+                follower[key] = (follower[key] * 1.0)/wordifies[key]
+            else:
+                follower[key] = 0.0000001
+    total_rt = 0
+    for row in user_tweets[user][0]:
+        row[1] = row[1]
+        row[2] = int(row[2])
+        row[3] = int(row[3])
+        total_rt += row[2]
+    user_tweet_wc = {}
+    for word in input_tweet:
+        user_tweet_wc[word] = 0
+    for word in user_tweet_wc:
+        for tweet in user_tweets[user][0]:
+            if word in tweet[1]:
+                user_tweet_wc[word] += tweet[2]
+    for key in user_tweet_wc:
+        user_tweet_wc[key] = (user_tweet_wc[key] * 1.0) / total_rt
+    #print user_tweet_wc
+    #print follower_wordified
+    rt_prob = []
+    for follower in follower_wordified:
+        foll_prob = 0
+        for word in follower:
+            foll_prob += follower[word] * user_tweet_wc[word]
+        rt_prob.append(foll_prob)
+    exp_rt_count = 0
+    for i in range(len(rt_prob)):
+        if rt_prob[i] > 0.001:
+            exp_rt_count += 1
+    return exp_rt_count
+
+#===================================================
+#Start of driver
+
+def main():
+    import sys
+    import pdb
+    from math import fabs
+    user = 'mulaney'#sys.argv[1]
+    user_tweets, follower_tweets = get_all_data(user)
+    #user, Train set: user_tweets[user][0], Test set: user_tweets[user][1]
+    #for each follower, Train set: follower_tweets[follower][0], Test set: follower_tweets[follower][1]
+    input_tweet = raw_input("Enter your tweet for analysis: ").lower().split()
+    exp_rt = classify(user, input_tweet, user_tweets, follower_tweets)
+    print "Possible number of retweets: " + str(exp_rt)
+    #pdb.set_trace()
+    total_exp_rt = 0
+    total_actual_rt = 0
+    for tweet in user_tweets[user][1]:
+        exp_test_rt = classify(user, tweet[1], user_tweets, follower_tweets)
+        print tweet[1]
+        print exp_test_rt
+        print tweet[2]
+        total_actual_rt += int(tweet[2])
+        total_exp_rt += exp_test_rt
+    total_err = fabs(total_actual_rt - total_exp_rt)
+    print "Error rate: " + str((total_err * 100.0)/total_actual_rt) + "%"
+
+    '''[os.remove(f) for f in glob('*.jpg')]
+    os.chdir('../')'''
+
+if __name__ == "__main__":
+    main()
--- myScraper.py
+++ myScraper.py
@@ -11,39 +11,85 @@ import webbrowser
 import pdb
 import tweepy
 import os
+import datetime as dt
+from nltk.corpus import stopwords
 
 
 def getTweets(usrNm, flag):
     """Collects the tweets of passed @username and returns a list of tweets"""
     usrUrl = 'https://twitter.com/' + usrNm.lower()
     src = sourceExtractor(usrUrl)
-    soup = BeautifulSoup(src)
+    soup = BeautifulSoup(src, 'html.parser')
     tweetStream = soup.find('ol', {'id': 'stream-items-id'}).find_all('li', {'data-item-type': 'tweet'})
-    #pdb.set_trace()
-    tweetdivs = []
-    for tweetTree in tweetStream:
-        tweetdivs.append(tweetTree.find('div', {'class': 'tweet'}))
-    tweetlist = []
-    data = open(flag + '_' + usrNm + '.csv', 'w')
-
-    for item in tweetdivs:
-        try:
-            content = item.find('div', {'class': 'content'}).find('p', {'class': 'tweet-text', 'lang': 'en'}).text.replace('\n', ' ').replace('"', '')
-            words = re.findall("[-'#@\w]+", content)
-            s = ''
-            for word in words:
-                s = s + word + ' '
-            content = s
-            timestamp = item.find('a', {'class': 'tweet-timestamp'})['title']
+    if len(tweetStream) < 1:
+        pass
+    else:
+        tweetdivs = []
+        for tweetTree in tweetStream:
+            tweetdivs.append(tweetTree.find('div', {'class': 'tweet'}))
+        tweetlist = []
+        anothertemplist = []
+        #pdb.set_trace()
+        for item in tweetdivs:
             try:
-                data.write(timestamp + '|' + content + '\n')
+                context = item.find('div', {'class': 'context'}).text
+                if flag == 'u':
+                    #if user, collect only tweets
+                    if 'Retweeted' in context:
+                        continue
+                    else:
+                        content = item.find('div', {'class': 'content'}).find('p', {'class': 'tweet-text', 'lang': 'en'}).text.replace('\n', ' ').replace('"', '')
+                        words = re.findall("[-'#@\w]+", content)
+                        cachedStops = stopwords.words("english")
+                        content = ' '.join([word for word in words if word not in cachedStops])
+                        timestamp = item.find('a', {'class': 'tweet-timestamp'})['title']
+                        footer = item.find('div', {'class': 'stream-item-footer'})
+                        retweet = footer.find('div', {'class': 'ProfileTweet-action--retweet'}).find('div', {'class': 'IconTextContainer'}).find('span', {'class': 'ProfileTweet-actionCountForPresentation'}).text
+                        if retweet == '':
+                            retweet = '0'
+                        favorite = footer.find('div', {'class': 'ProfileTweet-action--favorite'}).find('div', {'class': 'IconTextContainer'}).find('span', {'class': 'ProfileTweet-actionCountForPresentation'}).text
+                        if favorite == '':
+                            favorite = '0'
+                        tweet_tmstmp = dt.datetime.strptime(timestamp, '%I:%M %p - %d %b %Y')
+                        if ((dt.datetime.now() - tweet_tmstmp).total_seconds()/86400.0 > 28.0):
+                            continue
+                        else:
+                            anothertemplist.append([timestamp, content, retweet, favorite])
+                else:
+                    #if followers, collect only retweets
+                    if 'Retweeted' in context:
+                        content = item.find('div', {'class': 'content'}).find('p', {'class': 'tweet-text', 'lang': 'en'}).text.replace('\n', ' ').replace('"', '')
+                        words = re.findall("[-'#@\w]+", content)
+                        cachedStops = stopwords.words("english")
+                        content = ' '.join([word for word in words if word not in cachedStops])
+                        timestamp = item.find('a', {'class': 'tweet-timestamp'})['title']
+                        footer = item.find('div', {'class': 'stream-item-footer'})
+                        retweet = footer.find('div', {'class': 'ProfileTweet-action--retweet'}).find('div', {'class': 'IconTextContainer'}).find('span', {'class': 'ProfileTweet-actionCountForPresentation'}).text
+                        if retweet == '':
+                            retweet = '0'
+                        favorite = footer.find('div', {'class': 'ProfileTweet-action--favorite'}).find('div', {'class': 'IconTextContainer'}).find('span', {'class': 'ProfileTweet-actionCountForPresentation'}).text
+                        if favorite == '':
+                            favorite = '0'
+                        tweet_tmstmp = dt.datetime.strptime(timestamp, '%I:%M %p - %d %b %Y')
+                        if ((dt.datetime.now() - tweet_tmstmp).total_seconds()/86400.0 > 28.0):
+                            continue
+                        else:
+                            anothertemplist.append([timestamp, content, retweet, favorite])
+                    else:
+                        continue
             except:
                 pass
-        except:
-            pass
-    data.close()
-    print 'File wriiten: ' + data.name
-    #return tweets
+        if len(anothertemplist) != 0:
+            data = open(flag + '_' + usrNm + '.csv', 'w')
+            for row in anothertemplist:
+                timestamp, content, retweet, favorite = row
+                try:
+                    data.write(timestamp + '|' + content + '|' + retweet + '|' + favorite + '\n')
+                except:
+                    pass
+            data.close()
+            print 'File written: ' + data.name
+        #return tweets
 
 def sourceExtractor(url):
     """Get the source code of the passed url"""
@@ -52,12 +98,24 @@ def sourceExtractor(url):
     driver = webdriver.Firefox()
     driver.get(url)
     lensrc1 = driver.execute_script("return document.body.scrollHeight")
+    #pdb.set_trace()
     while(True):
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
         sleep(2)
         lensrc2 = driver.execute_script("return document.body.scrollHeight")
-        if(lensrc2 == lensrc1):
+        
+        if (lensrc2 == lensrc1):
             break
+        else:
+            try:
+                page_last_timestamp = BeautifulSoup(driver.page_source, 'html.parser').find('ol', {'id': 'stream-items-id'}).find_all('li', {'data-item-type': 'tweet'})[-1].find('div', {'class': 'tweet'}).find('a', {'class': 'tweet-timestamp'})['title']
+                pg_last_tmstmp = dt.datetime.strptime(page_last_timestamp, '%I:%M %p - %d %b %Y')
+                if ((dt.datetime.now() - pg_last_tmstmp).total_seconds()/86400) >= 28.0:
+                    break
+                else:
+                    pass
+            except:
+                pass
         lensrc1 = lensrc2
     src2 = driver.page_source
     driver.close()
@@ -68,7 +126,7 @@ def id_to_username(ID):
     usock = urllib2.urlopen('http://twitter.com/intent/user?user_id=' + str(ID))
     src = usock.read()
     usock.close()
-    soup = BeautifulSoup(src)
+    soup = BeautifulSoup(src, 'html.parser')
     return soup.find('span', {'class': 'nickname'}).text[1:]
 
 
@@ -90,17 +148,19 @@ tweepyUsr = api.get_user(usrNm)
 followers_ids = api.followers_ids(usrNm)
 followers_unames = [] 
 for ID in followers_ids:
-    followers_unames.append(str(id_to_username(ID)))
+    try:
+        followers_unames.append(str(id_to_username(ID)))
+    except:
+        pass
 #print followers_unames
 #print followers
-import threading
 for follower in followers_unames:
     try:
         getTweets(follower, 'f')
     except:
-        print follower
         pass
 os.chdir('../')
 infodata = open(usrNm + '.info', 'w')
 infodata.write(usrNm)
 infodata.close()
+
